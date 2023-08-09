ef83b2114e68b4b85d74b5fbb18a6011b2b09dcd
Riley Tomasek <riley.tomasek@gmail.com>
2023-08-09 16:06:16 -0400

    Build before publishing

--- publish.sh
+++ publish.sh
@@ -4,6 +4,7 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
+pnpm run build
 pnpm changeset version
 git add .
 git commit -m "Publish updated packages"
