d86910691a06862320b4cc8a9b583096ed3625e1
Riley Tomasek <riley.tomasek@gmail.com>
2023-09-05 10:02:54 -0400

    Export createPineconeClient function publicly

--- packages/datastore/src/pinecone/index.ts
+++ packages/datastore/src/pinecone/index.ts
@@ -1,3 +1,6 @@
 export { Datastore, HybridDatastore } from './datastore.js';
 
+export { createPineconeClient } from './client.js';
+export type { PineconeClient } from './client.js';
+
 export type { Doc, IDatastore } from '../types.js';
