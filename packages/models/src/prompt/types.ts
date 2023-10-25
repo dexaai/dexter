2575f4d8b8cb0df1f4faabf5c5edd0d784c8785e
Riley Tomasek <riley.tomasek@gmail.com>
2023-10-25 17:45:24 -0600

    Remove models package
    It has been merged into model now

--- packages/models/src/prompt/types.ts
+++
@@ -1,7 +0,0 @@
-import type { ChatModel } from '../chat/types.js';
-
-export namespace Prompt {
-  export type Template<T = Record<string, any>> = (
-    params: T
-  ) => Promise<ChatModel.Message[]> | ChatModel.Message[];
-}
--- packages/models/src/utils/type-helpers.ts
+++
@@ -1,2 +0,0 @@
-/** Improve preview of union types in autocomplete. */
-export type Prettify<T> = { [K in keyof T]: T[K] } & {};
