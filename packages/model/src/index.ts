c3d80cf47dd271f6974078adf828a3b5fa0d4f59
Riley Tomasek <riley.tomasek@gmail.com>
2023-10-26 16:48:05 -0600

    Move prompting tools from /model to /prompts

--- packages/model/src/index.ts
+++ packages/model/src/index.ts
@@ -3,7 +3,4 @@ export type { Model } from './types.js';
 export type { ModelArgs } from './model.js';
 export { AbstractModel } from './model.js';
 
-export { extractJsonObject } from './utils/extract-json.js';
-export { extractZodObject } from './utils/extract-zod-object.js';
 export { getMemoryCache } from './utils/memory-cache.js';
-export { zodToJsonSchema } from './utils/zod-to-json.js';
---
+++ packages/prompt/readme.md
@@ -0,0 +1,3 @@
+# LLM Prompt Tools
+
+Tools for prompting LLMs
