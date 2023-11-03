bd8b37b89eb5555d3084dfb62e15d61af9d8d289
Riley Tomasek <riley.tomasek@gmail.com>
2023-11-03 11:29:12 -0400

    Add missing ESLint config file
    This was accidentally deleted when moving from the monorepo.

--- src/datastore/types.ts
+++ src/datastore/types.ts
@@ -144,6 +144,7 @@ export namespace Dstore {
   }
 
   // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export type BaseFilter<Meta extends BaseMeta> = any;
 
   /**
