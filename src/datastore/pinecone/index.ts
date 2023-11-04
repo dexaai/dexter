eeaf162b307938f57c6ab07c8635bb73e0d89a77
Travis Fischer <fisch0920@gmail.com>
2023-11-04 18:49:35 -0500

    fix: renamed exports

--- src/datastore/pinecone/index.ts
+++ src/datastore/pinecone/index.ts
@@ -1,5 +1,5 @@
-export { PineconeDatastore as Datastore } from './datastore.js';
-export { PineconeHybridDatastore as HybridDatastore } from './hybrid-datastore.js';
+export { PineconeDatastore } from './datastore.js';
+export { PineconeHybridDatastore } from './hybrid-datastore.js';
 
 export { createPineconeClient } from './client.js';
 export type { PineconeClient } from './client.js';
