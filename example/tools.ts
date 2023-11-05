dfbb6c3210f05181f18f0c3b863a2bd1c2e81ecb
Riley Tomasek <riley.tomasek@gmail.com>
2023-11-04 23:21:09 -0400

    Add Hybrid RAG chat with Huberman CLI example

---
+++ example/tools.ts
@@ -0,0 +1,41 @@
+import { PineconeHybridDatastore } from '../src/datastore/pinecone/hybrid-datastore.js';
+import { ChatModel } from '../src/model/chat.js';
+import { EmbeddingModel } from '../src/model/embedding.js';
+import { SparseVectorModel } from '../src/model/sparse-vector.js';
+
+export interface Chunk {
+  chunkId: string;
+  chunkTitle: string;
+  episodeTitle: string;
+  imgUrl: string;
+  published: string;
+  url: string;
+  transcript: string;
+}
+
+const embeddingModel = new EmbeddingModel({
+  params: { model: 'text-embedding-ada-002' },
+});
+
+const spladeModel = new SparseVectorModel({
+  params: {
+    model: 'naver/splade-cocondenser-ensembledistil',
+    concurrency: 30,
+  },
+});
+
+export const chunkDatastore = new PineconeHybridDatastore<Chunk>({
+  namespace: 'test12',
+  contentKey: 'transcript',
+  embeddingModel,
+  spladeModel,
+});
+
+export const chatModel = new ChatModel({
+  debug: true,
+  params: {
+    model: 'gpt-4',
+    temperature: 0.5,
+    max_tokens: 200,
+  },
+});
