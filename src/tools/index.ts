2ec1df387290b03b1d2cb3bc45d27c3c4495034f
Travis Fischer <fisch0920@gmail.com>
2023-12-01 04:37:05 -0600

    💏

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,4 +1,6 @@
 export * from './fns.js';
 export * from './tool-set.js';
 export * from './function-set.js';
+export * from './services/serpapi.js';
 export * from './services/serper.js';
+export * from './services/weather.js';
