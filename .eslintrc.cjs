b64b9a883f4a0f0cc15be189c4d3468e530ed4c4
Travis Fischer <fisch0920@gmail.com>
2024-08-09 00:25:43 -0500

    fix: eslint CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+  root: true,
   extends: ['@dexaai/eslint-config', '@dexaai/eslint-config/node'],
   ignorePatterns: ['.next/', 'dist/', 'node_modules/'],
   rules: {
